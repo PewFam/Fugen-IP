6cb3e953d95ad41a3eedbf98c3de9065cb281770
PewFam <106314076+PewFam@users.noreply.github.com>
2022-06-12 15:11:16 +0200

    Update Fugen-IP.py

--- Fugen-IP.py
+++ Fugen-IP.py
@@ -23,10 +23,10 @@ header = """
 print(Colorate.Vertical(Colors.green_to_blue, Center.Center(header)))
 list3 = [1,2,3,5,6,7,8,9]
 list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-list2 = [""]
+list2 = ["", 1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 a = input('\nHow many ip ? % ')
 print('\n\n')
 for a in range(int(a)):
     
-    print(Colorate.Diagonal(Colors.red_to_yellow , (str(random.choice(list3)) + str(random.choice(list3)) + "." + str(random.choice(list1)) + str(random.choice(list1)) + str(random.choice(list1)) +"." + str(random.choice(list1)) +str(random.choice(list1)) +"." + str(random.choice(list1))+str(random.choice(list1)) )))
+    print(Colorate.Diagonal(Colors.red_to_yellow , (str(random.choice(list3)) + str(random.choice(list3)) + "." + str(random.choice(list1)) + str(random.choice(list1)) + str(random.choice(list1)) +"." + str(random.choice(list1)) +str(random.choice(list1)) +"." + str(random.choice(list1))+str(random.choice(list2)) )))
     print()
